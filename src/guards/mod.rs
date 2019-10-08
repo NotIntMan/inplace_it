3f35c71f6decae38a4b0a1bacae3cb8d82d0929a
Dmitry Demin <shepardiwe@gmail.com>
2019-10-08 11:58:30 +0700

    Removing rudiments; Adding .idea files.

--- src/guards/mod.rs
+++ src/guards/mod.rs
@@ -1,9 +1,5 @@
-mod uninitialized_memory_guard;
 mod uninitialized_slice_memory_guard;
-mod memory_guard;
 mod slice_memory_guard;
 
-pub use uninitialized_memory_guard::*;
 pub use uninitialized_slice_memory_guard::*;
-pub use memory_guard::*;
 pub use slice_memory_guard::*;
